cbc24959c6e4f5ba83880409fa3c71ccfb35a7e6
SamikshaSinha-31 <sinhasamiksha04@gmail.com>
2024-09-30 23:37:21 +0530

    Create sum-of-digits.java

---
+++ Recursion-Basics/sum-of-digits.java
@@ -0,0 +1,29 @@
+/*Description: To find the sum of digits of a number
+  Time complexity:O(logn)
+  */
+package com.dsa;
+public class sum_of_digits{
+//function call
+  private int sum(int n){
+    //base case
+    if(n==0){
+      return 0;
+    }
+    //recursive call
+    return sum(n%10 + sum(n/10));
+  }
+
+  //main function
+  public static void main(String[]args){
+    int n=123;
+    int s=sum(n);
+    System.out.print("Sum of digits of" + n + "is " + a);
+  }
+}
+
+/*Sample input:
+  n=123
+  Sample output:
+  Sum of digits of 123 is 6
+  */
+
