869875130acfcb284ad730f9c9c93cc7911f737b
SamikshaSinha-31 <sinhasamiksha04@gmail.com>
2024-10-08 21:03:47 +0530

    Update Count-digits-of-a-number.java

--- Recursion-Basics/Count-digits-of-a-number.java
+++ Recursion-Basics/Count-digits-of-a-number.java
@@ -1,1 +1,33 @@
+package com.dsa;
+/*Description: To count digits of a number using recursion.
 
+Time Complexity:  O(log10(N)) or O(Number of Digits), where N is the input number.
+*/
+public class CountDigits{
+
+  //function starts
+  private static int count(int n){
+    //base case
+    if(n/10==0){
+      return 1;
+    }
+    //recursive call
+    else{
+      return (1+count(n/10));
+    }
+  }
+
+//main function
+public static void main(String[]args){
+   int n=234;
+   System.out.println("Number of digits in " + n + " is: " + count(n));
+  }
+
+    /*
+    Sample Input:
+    n=234
+
+    Output:
+    Number of digits in 234 is: 3
+    */
+}
