0249fa80b5ad33347f83dc3831136b6935df6828
SamikshaSinha-31 <sinhasamiksha04@gmail.com>
2025-07-08 13:38:21 +0530

    Create Regular-Expression-Matching.java

---
+++ LeetCode/Regular-Expression-Matching.java
@@ -0,0 +1,36 @@
+/*Problem Number: 10. Regular Expression Matching
+Problem Statement: Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Example 1:
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+*/
+
+class Solution {
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match =(!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) ||
+                    pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (
+                isMatch(text, pattern.substring(2)) ||
+                (first_match && isMatch(text.substring(1), pattern))
+            );
+        } else {
+            return (
+                first_match && isMatch(text.substring(1), pattern.substring(1))
+            );
+        }
+    }
+}
